b97c98a001db996ce0243898d1a21c05ec141844
jackipro <48981897+jackipro@users.noreply.github.com>
2019-09-08 05:17:52 +0700

    Delete baitap.js

--- baitap.js
+++
@@ -1,7 +0,0 @@
-process.stdin.once('data', (input) => {
-     var items = input.toString().trim().split(' ');
-     var a = parseInt(items[0]);
-     var b = parseInt(items[1]);
-     console.log(a + b);
-     process.exit();
-});
