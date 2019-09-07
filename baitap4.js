d67b92a879f5f5f873a9ac4abb77b15eaffcf19b
jackipro <48981897+jackipro@users.noreply.github.com>
2019-09-08 05:18:26 +0700

    Delete baitap4.js

--- baitap4.js
+++
@@ -1,9 +0,0 @@
-process.stdin.once('data', (input) =>{
-    var items = input.toString().trim().split(' ');
-    var a = parseInt(items[0]);
-    var b = parseInt(items[1]);
-    var chuVi = (a + b) * 2 ;
-    var dienTich = a * b ;
-    console.log(chuVi + " " + dienTich);
-    process.exit();
-});
