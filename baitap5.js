631fabb9b657e84360c3cf06fff573767f4c0459
jackipro <48981897+jackipro@users.noreply.github.com>
2019-09-08 05:18:36 +0700

    Delete baitap5.js

--- baitap5.js
+++
@@ -1,7 +0,0 @@
-process.stdin.once('data' , (input) =>{
-    var R = parseFloat(input);
-    var S = (4*(Math.PI.toFixed(2))) * Math.pow(R, 2);
-    var V = (4/3) * (Math.PI.toFixed(2)) * Math.pow(R,3);
-    console.log(S.toFixed(2) + " " + V.toFixed(2));
-    process.exit();
-});
