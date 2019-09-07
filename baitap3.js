ac0f1b17c85611659b407135fc3e2147b64e3202
jackipro <48981897+jackipro@users.noreply.github.com>
2019-09-08 05:18:14 +0700

    Delete baitap3.js

--- baitap3.js
+++
@@ -1,8 +0,0 @@
-process.stdin.once('data' , (input) =>{
-        var a = parseInt(input);
-        var aMuHai = Math.pow(a, 2);
-        var aMuBa = Math.pow(a, 3);
-        var aMuBon = Math.pow(a, 4);
-        console.log(aMuHai + " " + aMuBa + " " + aMuBon );
-        process.exit();
-});
